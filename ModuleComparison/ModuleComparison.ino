6af858a6a0101e275febc0462fc0587022d01b5e
Kobe Dieryck <kobe.dieryck@icloud.com>
2024-05-21 16:19:22 +0200

    cheese

--- ModuleComparison/ModuleComparison.ino
+++ ModuleComparison/ModuleComparison.ino
@@ -5,17 +5,34 @@ SensorClass sensors;
 
 void setup() {
   Serial.begin(9600); 
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.print("starting init\n\r");
   sensors.initialize();
+  Serial.print("init complete\n\r");
+  
 
 }
 
 void loop() {
+  digitalWrite(LED_BUILTIN, LOW);
   sensors.readData();
   sensors.processData();
-  sensors.printData(sensors.getsensorData(SPS30_ID));
-  sensors.printData(sensors.getsensorData(MHZ19_ID));
-  sensors.printData(sensors.getsensorData(SCD30_ID));
-  sensors.printData(sensors.getsensorData(PPD42_ID));
+  #ifdef Use_SPS30
+    sensors.printData(sensors.getsensorData(SPS30_ID));
+  #endif
+
+  #ifdef Use_MHZ19
+    sensors.printData(sensors.getsensorData(MHZ19_ID));
+  #endif
+
+  #ifdef Use_SCD30
+    sensors.printData(sensors.getsensorData(SCD30_ID));
+  #endif
+
+  #ifdef Use_PPD42
+    sensors.printData(sensors.getsensorData(PPD42_ID));
+  #endif
 
 
 
--- ModuleComparison/SensorClass.hpp
+++ ModuleComparison/SensorClass.hpp
@@ -6,11 +6,24 @@
 #include "MHZ19.h"                                        
 #include "SparkFun_SCD30_Arduino_Library.h"
 #include <Adafruit_SCD30.h>
+#include <SoftwareSerial.h>
+
+const byte RX_pin = 2;	
+const byte TX_pin = 3;
+SoftwareSerial mySerial(RX_pin, TX_pin); 
+#define PPD_PIN1 4
+
 
 #define SPS30_ID 1
 #define MHZ19_ID 2
 #define SCD30_ID 3
 #define PPD42_ID 4
+
+//#define Use_SPS30
+//#define Use_MHZ19
+#define Use_SCD30
+//#define Use_PPD42
+
 
 typedef struct
 {
@@ -22,46 +35,61 @@ typedef struct
 // Sensor data struct
 typedef struct
 {
-    uint8_t humidity = NULL;
-    int8_t temperature = NULL;
-    int pressure = NULL;
-    int co2 = NULL;
+    uint8_t humidity = (uint8_t)0;
+    int8_t temperature = (uint8_t)0;
+    int pressure = (uint8_t)0;
+    int co2 = (uint8_t)0;
     ParticleData particles;
 }sensorData;
 
 class SensorClass {
 public:
     // Constructor
-    SensorClass(){};
+    SensorClass() {};
 
     // Destructor
-    ~SensorClass(){};
+    ~SensorClass() {};
 
     // Member functions
-    void initialize();// Initialize sensors
-    void readData();// Read data from sensors
-    void processData();// Process data from sensors
-    void printData(sensorData input);// Print data from sensor
+    void initialize(); // Initialize sensors
+    void readData(); // Read data from sensors
+    void processData(); // Process data from sensors
+    void printData(sensorData input); // Print data from sensor
     sensorData getsensorData(int sensorID);
     // sensorid 1 = SPS30, 2 = MHZ19, 3 = SCD30 4 = PPD42
-    
+
 private:
     sensorData SPS30_struct;
+    sensorData SCD30_struct;
+    sensorData MHZ19_struct;
+    sensorData PPD42_struct;
+
+    void initializeSPS30();
+    void initializeMHZ19();
+    void initializeSCD30();
+    void initializePPD42();
+
+    void readDataSPS30();
+    void readDataMHZ19();
+    void readDataSCD30();
+    void readDataPPD42();
+
+    void processDataSPS30();
+    void processDataMHZ19();
+    void processDataSCD30();
+    void processDataPPD42();
+
+    
     int16_t ret;
     struct sps30_measurement m;
     char serial[SPS30_MAX_SERIAL_LEN];
     uint16_t data_ready;
 
-    sensorData SCD30_struct;
     Adafruit_SCD30 scd30;
 
-    sensorData MHZ19_struct;
     MHZ19 myMHZ19; 
     
-    
-
-    sensorData PPD42_struct;
-    int pin = 8; 
+    int PPD_pin = PPD_PIN1; 
     unsigned long duration;
     unsigned long starttime;
     unsigned long sampletime_ms = 2000; 
@@ -71,8 +99,68 @@ private:
 };
 
 void SensorClass::initialize() {
-    // SPS30
-  sensirion_i2c_init();
+    #ifdef Use_SPS30
+    initializeSPS30();
+    #endif
+    #ifdef Use_MHZ19
+    initializeMHZ19();
+    #endif
+    #ifdef Use_SCD30
+    initializeSCD30();
+    #endif
+    #ifdef Use_PPD42
+    initializePPD42();
+    #endif
+}
+
+void SensorClass::readData() {
+    #ifdef Use_SPS30
+    readDataSPS30();
+    #endif
+    #ifdef Use_MHZ19
+    readDataMHZ19();
+    #endif
+    #ifdef Use_SCD30
+    readDataSCD30();
+    #endif
+    #ifdef Use_PPD42
+    readDataPPD42();
+    #endif
+}
+
+void SensorClass::processData() {
+    #ifdef Use_SPS30
+    processDataSPS30();
+    #endif
+    #ifdef Use_MHZ19
+    processDataMHZ19();
+    #endif
+    #ifdef Use_SCD30
+    processDataSCD30();
+    #endif
+    #ifdef Use_PPD42
+    processDataPPD42();
+    #endif
+}
+
+sensorData SensorClass::getsensorData(int sensorID) {
+    switch (sensorID) {
+        case SPS30_ID:
+            return SPS30_struct;
+        case MHZ19_ID:
+            return MHZ19_struct;
+        case SCD30_ID:
+            return SCD30_struct;
+        case PPD42_ID:
+            return PPD42_struct;
+        default:
+            break;
+    }
+}
+
+void SensorClass::initializeSPS30() {
+    // SPS30 initialization code
+    sensirion_i2c_init();
   while (sps30_probe() != 0) {
     Serial.print("SPS sensor probing failed\n");
     delay(500);
@@ -81,67 +169,82 @@ void SensorClass::initialize() {
   if (ret < 0) {
     Serial.print("error starting measurement\n");
   }
-  delay(1000);
+}
 
-    // MHZ19
-    myMHZ19.begin(Serial);
+void SensorClass::initializeMHZ19() {
+    // MHZ19 initialization code
+    myMHZ19.begin(mySerial);
     myMHZ19.autoCalibration();
+}
 
-    // SCD30
-    if (!scd30.begin()) {
+void SensorClass::initializeSCD30() {
+    // SCD30 initialization code
+     if (!scd30.begin()) {
     Serial.println("Failed to find SCD30 chip");
     while (1) { delay(10); }
     }
-    Serial.println("SCD30 Found!");
+}
 
-    // PPD42
-    pinMode(pin, INPUT);
+void SensorClass::initializePPD42() {
+    // PPD42 initialization code
+    pinMode(PPD_pin, INPUT);
     starttime = millis(); 
 }
 
-void SensorClass::readData() {
-    // Read data from sensor
-    // SPS30
-sps30_start_measurement(); // Start of loop start fan to flow air past laser sensor
-  delay(4000); //Wait 1 second while fan is active before read data
+void SensorClass::readDataSPS30() {
+    // SPS30 read data code
+    sps30_start_measurement(); // Start of loop start fan to flow air past laser sensor
+    delay(4000); //Wait 1 second while fan is active before read data
 
-  do {
-    ret = sps30_read_data_ready(&data_ready); // Reads the last data from the sensor
-    if (ret < 0) {
-      Serial.print("error reading data-ready flag: ");
-      Serial.println(ret);
-    } else if (!data_ready)
-      Serial.print("data not ready, no new measurement available\n");
-    else
-      break;
-    delay(100); /* retry in 100ms */
-  } while (1);
-  ret = sps30_read_measurement(&m); // Ask SPS30 for measurments over I2C, returns 10 sets of data
+    do {
+        ret = sps30_read_data_ready(&data_ready); // Reads the last data from the sensor
+        if (ret < 0) {
+            Serial.print("error reading data-ready flag: ");
+            Serial.println(ret);
+        } else if (!data_ready)
+            Serial.print("data not ready, no new measurement available\n");
+            else
+            break;
+            delay(100); /* retry in 100ms */
+    } while (1);
+        ret = sps30_read_measurement(&m); // Ask SPS30 for measurments over I2C, returns 10 sets of data
+}
 
-    // SCD30
+void SensorClass::readDataMHZ19() {
+    // MHZ19 read data code
+    
+}
+
+void SensorClass::readDataSCD30() {
+    // SCD30 read data code
     if (scd30.dataReady()) {
         if (!scd30.read()) {
             Serial.println("Error reading sensor data");
             return;
         }
     }
+}
 
-    // PPD42
-    duration = pulseIn(pin, LOW);
+void SensorClass::readDataPPD42() {
+    // PPD42 read data code
+    duration = pulseIn(PPD_pin, LOW);
     lowpulseoccupancy = lowpulseoccupancy + duration;
-}   
+}
 
-void SensorClass::processData() {
-    // Process data from sensor
-    // SPS30
+void SensorClass::processDataSPS30() {
+    // SPS30 process data code
     SPS30_struct.particles.pm2_5 = m.mc_2p5;
     SPS30_struct.particles.pm10_0 = m.mc_10p0;
+}
 
-    // MHZ19
+void SensorClass::processDataMHZ19() {
+    // MHZ19 process data code
     MHZ19_struct.co2 = myMHZ19.getCO2(); 
     MHZ19_struct.temperature = myMHZ19.getTemperature();
+}
 
-    // SCD30
+void SensorClass::processDataSCD30() {
+    // SCD30 process data code
     if (scd30.dataReady()) {
         SCD30_struct.temperature = scd30.temperature;
         SCD30_struct.co2 = scd30.CO2;
@@ -149,8 +252,10 @@ void SensorClass::processData() {
     } else {
         Serial.println("SCD30 - Data not available");
     }
+}
 
-    // PPD42
+void SensorClass::processDataPPD42() {
+    // PPD42 process data code
     if ((millis() - starttime) >= sampletime_ms) { // if the sample time == 2s
         ratio = lowpulseoccupancy / (sampletime_ms * 10.0);  
         concentration = 1.1 * pow(ratio, 3) - 3.8 * pow(ratio, 2) + 520 * ratio + 0.62; 
@@ -163,53 +268,39 @@ void SensorClass::processData() {
 void SensorClass::printData(sensorData input) {
     // Print data from sensor
     // particle sensor:
-    if (input.particles.pm2_5 != NULL) {
+    if (input.particles.pm2_5 != 0) {
         Serial.print("PM  2.5: ");
         Serial.println(input.particles.pm2_5);
     }
 
-    if (input.particles.pm10_0 != NULL) {
+    if (input.particles.pm10_0 != 0) {
         Serial.print("PM 10.0: ");
         Serial.println(input.particles.pm10_0);
     }
 
-    if (input.particles.pcs_0_01cf != NULL) {
+    if (input.particles.pcs_0_01cf != 0) {
         Serial.print("Concentration: ");
         Serial.print(input.particles.pcs_0_01cf);
         Serial.println(" pcs/0.01cf"); // Print concentration with unit
     }
 
     // CO2 and temperature sensor:
-    if (input.co2 != NULL) {
+    if (input.co2 != 0) {
         Serial.print("CO2 (ppm): ");                      
         Serial.println(input.co2);
     }
 
-    if (input.temperature != NULL) {
+    if (input.temperature != 0) {
         Serial.print("Temperature (C): ");
         Serial.println(input.temperature);
     }
 
     // pressure sensor:
-    if (input.pressure != NULL) {
+    if (input.pressure != 0) {
         Serial.print("Pressure (Pa): ");
         Serial.println(input.pressure);
     } 
 }
 
-sensorData SensorClass::getsensorData(int sensorID) {
-    switch (sensorID) {
-        case 1:
-            return SPS30_struct;
-        case 2:
-            return MHZ19_struct;
-        case 3:
-            return SCD30_struct;
-        case 4:
-            return PPD42_struct;
-        default:
-            break;
-    }
-}
 
 #endif // SENSORCLASS_HPP
